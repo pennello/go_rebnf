be9d17b44b9dd43f56589c4f938258e50ca09964
Chris Pennello <git@chrispennello.com>
2015-07-28 10:32:40 -0700

    Updates random docs & library usage.
    Adds documentation.
    Now uses chrispennello.com/go/fix/math/rand random functions instead of
    local util random functions.  Removes local util random functions.

--- random.go
+++ random.go
@@ -4,15 +4,21 @@ import (
 	"errors"
 	"io"
 
-	"math/rand"
+	mathrand "math/rand"
 
+	fixrand "chrispennello.com/go/util/fix/math/rand"
 	"golang.org/x/exp/ebnf"
 )
 
+// ErrNoStart is returned by Random when the specified start production
+// cannot be found in the grammar.
 var ErrNoStart = errors.New("start production not found")
 
 const maxRepetitions = 100
 
+// Random generates random productions of the given grammar starting at
+// the given start production, and writes them into the destination
+// io.Writer.
 func Random(dst io.Writer, grammar ebnf.Grammar, start string) error {
 	prod, ok := grammar[start]
 	if !ok {
@@ -26,7 +32,7 @@ func random(dst io.Writer, grammar ebnf.Grammar, expr ebnf.Expression) error {
 	case ebnf.Alternative:
 		// Choose a random alternative.
 		alt := expr.(ebnf.Alternative)
-		err := random(dst, grammar, alt[rand.Intn(len(alt))])
+		err := random(dst, grammar, alt[mathrand.Intn(len(alt))])
 		if err != nil {
 			return err
 		}
@@ -49,7 +55,7 @@ func random(dst io.Writer, grammar ebnf.Grammar, expr ebnf.Expression) error {
 	case *ebnf.Option:
 		// Randomly include the option.
 		opt := expr.(*ebnf.Option)
-		if 0 == rand.Intn(2) { // 50/50 chance.
+		if fixrand.Bool() {
 			if err := random(dst, grammar, opt.Body); err != nil {
 				return err
 			}
@@ -64,7 +70,7 @@ func random(dst io.Writer, grammar ebnf.Grammar, expr ebnf.Expression) error {
 
 	case *ebnf.Range:
 		rng := expr.(*ebnf.Range)
-		ch, err := ChooseString(rng.Begin.String, rng.End.String)
+		ch, err := fixrand.ChooseString(rng.Begin.String, rng.End.String)
 		if err != nil {
 			return err
 		}
@@ -75,7 +81,7 @@ func random(dst io.Writer, grammar ebnf.Grammar, expr ebnf.Expression) error {
 	case *ebnf.Repetition:
 		// Choose a random number of repetitions.
 		rep := expr.(*ebnf.Repetition)
-		for i := 0; i < rand.Intn(maxRepetitions + 1); i++ {
+		for i := 0; i < mathrand.Intn(maxRepetitions+1); i++ {
 			if err := random(dst, grammar, rep.Body); err != nil {
 				return err
 			}

22f8d34af8e64244742567766fc0d0889fccc11c
Chris Pennello <git@chrispennello.com>
2015-07-29 16:50:37 -0700

    Implements recursion depth limit.

--- random.go
+++ random.go
@@ -5,18 +5,23 @@ package rebnf
 import (
 	"errors"
 	"io"
+	"unicode"
 
 	mathrand "math/rand"
+	"unicode/utf8"
 
 	fixrand "chrispennello.com/go/util/fix/math/rand"
 	"golang.org/x/exp/ebnf"
 )
 
+const (
+	maxRepetitions = 100
+	maxRecursionDepth = 100
+)
+
 // ErrNoStart is returned by Random when the specified start production
 // cannot be found in the grammar.
 var ErrNoStart = errors.New("start production not found")
-
-const maxRepetitions = 100
 
 // Random generates random productions of the given grammar starting at
 // the given start production, and writes them into the destination
@@ -26,15 +31,70 @@ func Random(dst io.Writer, grammar ebnf.Grammar, start string) error {
 	if !ok {
 		return ErrNoStart
 	}
-	return random(dst, grammar, prod.Expr)
+	return random(dst, grammar, prod.Expr, 0)
 }
 
-func random(dst io.Writer, grammar ebnf.Grammar, expr ebnf.Expression) error {
+// IsCapital returns a boolean indicating whether or not the first rune
+// of the given string is upper case.
+func IsCapital(s string) bool {
+	ch, _ := utf8.DecodeRuneInString(s)
+	return !unicode.IsUpper(ch)
+}
+
+// IsTerminal returns a boolean that indicates whether the given
+// Expression is a terminal one.  Ranges and Tokens are unconditionally
+// considered to be terminal, and Names are terminal iff they're
+// capitalized.  Productions are not considered because Alternatives
+// contain Names, and you have to loo up the production by name in the
+// grammar--it's just not a use case handled by this library, but could
+// be added easily if needed.
+func IsTerminal(expr ebnf.Expression) bool {
+	switch expr.(type) {
+	case *ebnf.Name:
+		name := expr.(*ebnf.Name)
+		return !IsCapital(name.String)
+	case *ebnf.Range:
+		return true
+	case *ebnf.Token:
+		return true
+	default:
+		return false
+	}
+}
+
+// findTerminals is like filter(IsTerminal, exprs).  It ranges over all
+// of the given expressions and produces a new slice of expressions
+// containing only those for which IsTerminal returns true.
+func findTerminals(exprs []ebnf.Expression) []ebnf.Expression {
+	r := make([]ebnf.Expression, 0, len(exprs))
+	for _, expr := range exprs {
+		if IsTerminal(expr) {
+			r = append(r, expr)
+		}
+	}
+	return r
+}
+
+func random(dst io.Writer, grammar ebnf.Grammar, expr ebnf.Expression, depth int) error {
 	switch expr.(type) {
 	case ebnf.Alternative:
 		// Choose a random alternative.
 		alt := expr.(ebnf.Alternative)
-		err := random(dst, grammar, alt[mathrand.Intn(len(alt))])
+		var exprs []ebnf.Expression
+		// If maximum recursion depth has been exceeded, attempt
+		// to select from only terminal expressions.
+		if depth > maxRecursionDepth {
+			exprs = findTerminals(alt)
+			if len(exprs) == 0 {
+				// No luck, we have no choice but to
+				// explore one of the non-terminals in
+				// this alternative.
+				exprs = alt
+			}
+		} else {
+			exprs = alt
+		}
+		err := random(dst, grammar, exprs[mathrand.Intn(len(exprs))], depth + 1)
 		if err != nil {
 			return err
 		}
@@ -42,7 +102,8 @@ func random(dst io.Writer, grammar ebnf.Grammar, expr ebnf.Expression) error {
 	case *ebnf.Group:
 		// Evalute the group.
 		gr := expr.(*ebnf.Group)
-		if err := random(dst, grammar, gr.Body); err != nil {
+		err := random(dst, grammar, gr.Body, depth + 1)
+		if err != nil {
 			return err
 		}
 
@@ -50,15 +111,23 @@ func random(dst io.Writer, grammar ebnf.Grammar, expr ebnf.Expression) error {
 		// The name refers to a production; look it up and
 		// continue the recursion.
 		name := expr.(*ebnf.Name)
-		if err := random(dst, grammar, grammar[name.String]); err != nil {
+		err := random(dst, grammar, grammar[name.String], depth + 1)
+		if err != nil {
 			return err
 		}
 
 	case *ebnf.Option:
 		// Randomly include the option.
 		opt := expr.(*ebnf.Option)
-		if fixrand.Bool() {
-			if err := random(dst, grammar, opt.Body); err != nil {
+		// If recursion depth has been exceeded, and option is
+		// non-termainl, unconditionally omit.
+		if depth > maxRecursionDepth && !IsTerminal(opt.Body) {
+			// Omit.
+		} else if fixrand.Bool() {
+			// Otherwise, proceed with usual random
+			// inclusion of option.
+			err := random(dst, grammar, opt.Body, depth + 1)
+			if err != nil {
 				return err
 			}
 		}
@@ -66,7 +135,8 @@ func random(dst io.Writer, grammar ebnf.Grammar, expr ebnf.Expression) error {
 	case *ebnf.Production:
 		// Produce the production.
 		prod := expr.(*ebnf.Production)
-		if err := random(dst, grammar, prod.Expr); err != nil {
+		err := random(dst, grammar, prod.Expr, depth + 1)
+		if err != nil {
 			return err
 		}
 
@@ -83,9 +153,18 @@ func random(dst io.Writer, grammar ebnf.Grammar, expr ebnf.Expression) error {
 	case *ebnf.Repetition:
 		// Choose a random number of repetitions.
 		rep := expr.(*ebnf.Repetition)
-		for i := 0; i < mathrand.Intn(maxRepetitions+1); i++ {
-			if err := random(dst, grammar, rep.Body); err != nil {
-				return err
+		// If the recursion depth has been exceeded, and the
+		// repetition is non-terminal, unconditionally omit it.
+		if depth > maxRecursionDepth && !IsTerminal(rep.Body) {
+			// Omit.
+		} else {
+			// Otherwise, do normal inclusion of a random
+			// number of repetitions.
+			for i := 0; i < mathrand.Intn(maxRepetitions+1); i++ {
+				err := random(dst, grammar, rep.Body, depth + 1)
+				if err != nil {
+					return err
+				}
 			}
 		}
 
@@ -93,7 +172,8 @@ func random(dst io.Writer, grammar ebnf.Grammar, expr ebnf.Expression) error {
 		// Recurse on each of the expressions.
 		seq := expr.(ebnf.Sequence)
 		for _, e := range seq {
-			if err := random(dst, grammar, e); err != nil {
+			err := random(dst, grammar, e, depth + 1)
+			if err != nil {
 				return err
 			}
 		}
--- readme.md
+++ readme.md
@@ -2,8 +2,6 @@ TODO
 ----
  - On error, output chosen seed.  Accept seed as an input so you can
    reproduce errors.
- - Bound output.  Recursion depth limit?  Use lower-case production
-   names for terminal symbols.
  - Document how library does not avoid infinite productions (i.e.,
    `testlang/inf`).
  - Context for `maxRepetitions` and recursion depth limit to allow

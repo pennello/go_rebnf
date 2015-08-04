b09dc7d338d287b380f3f1cc15339ff9ff588872
Chris Pennello <git@chrispennello.com>
2015-08-04 10:12:04 -0700

    Adds test for IsTerminal.

--- random_test.go
+++ random_test.go
@@ -4,6 +4,8 @@ package rebnf
 
 import (
 	"testing"
+
+	"golang.org/x/exp/ebnf"
 )
 
 func testIsCapital(t *testing.T, x string, expect bool) {
@@ -17,4 +19,32 @@ func TestIsCapital(t *testing.T) {
 	testIsCapital(t, "there", false)
 	testIsCapital(t, "", false)
 	testIsCapital(t, "Σ1", true)
+}
+
+func testIsTerminal(t *testing.T, expr ebnf.Expression, expect bool) {
+	if IsTerminal(expr) != expect {
+		t.Fail()
+	}
+}
+
+func TestIsTerminal(t *testing.T) {
+	var expr ebnf.Expression
+	var t1, t2 ebnf.Token
+
+	expr = &ebnf.Name{String: "Production"}
+	testIsTerminal(t, expr, false)
+
+	expr = &ebnf.Name{String: "token"}
+	testIsTerminal(t, expr, true)
+
+	expr = &ebnf.Token{String: "blah"}
+	testIsTerminal(t, expr, true)
+
+	t1 = ebnf.Token{String: "a"}
+	t2 = ebnf.Token{String: "z"}
+	expr = &ebnf.Range{Begin: &t1, End: &t2}
+	testIsTerminal(t, expr, true)
+
+	expr = &ebnf.Repetition{Body: &ebnf.Token{String: "foo"}}
+	testIsTerminal(t, expr, false)
 }
